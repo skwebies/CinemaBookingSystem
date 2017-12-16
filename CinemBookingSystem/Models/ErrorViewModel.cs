5427738c1a358df6e1d1fc11798ad481ab43e3e2
Thineskumar Thilakanathan <skwebbies@hotmail.co.uk>
2017-12-16 13:54:02 +0100

    Add project files.

---
+++ CinemBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinemBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
